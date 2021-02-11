7bbf2147a2530af4135c0b95f4f3590371b2b985
tupacsharp@hotmail.com <tupacsharp@hotmail.com>
2021-02-11 03:25:57 +0200

    add video

--- src/index.js
+++ src/index.js
@@ -5,10 +5,19 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Bus from './components/bus'
 import 'bootstrap/dist/css/bootstrap.css';
-
+function getBaseUrl(){
+  var getUrl = window.location;
+var baseUrl =getUrl.protocol +"//" +getUrl.host +"/" +getUrl.pathname.split("/")[1];
+return baseUrl ;
+};
 ReactDOM.render(
   <React.StrictMode>
+    <div>
     <Bus/>
+    <video width="750" height="500" controls >
+    <source src={getBaseUrl()+"/chip8guide.mp4"} type="video/mp4"/>
+    </video>
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
